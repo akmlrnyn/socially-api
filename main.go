80281f6b8be905ae3807869588d7816b3837c3ec
akmlrnyn <akmalranyan@gmail.com>
2024-05-21 20:52:41 +0700

    setting up gin

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main () {
-	
+import (
+	"fmt"
+	"gin_social/configs"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	configs.LoadConfig()
+	configs.LoadDB()
+
+
+	r := gin.Default()
+	api := r.Group("/api")
+	api.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(fmt.Sprintf(":%v", configs.ENV.PORT)) // listen and serve on 0.0.0.0:8080
 }
